22cedfd18ba21b5079d2d5ea0737541f8d38fd3b
Asady <fayyadh.asady04@gmail.com>
2023-08-25 10:05:53 +0700

    MC run animation
    50%
    belum nemu kondisi yg cocok buat balik ke idle, ada bug animasi juga

--- Assets/Programs/PlayerScript.cs
+++ Assets/Programs/PlayerScript.cs
@@ -19,12 +19,15 @@ public class PlayerScript : MonoBehaviour
 {
 
     public Items[] inventory;
-    private Vector2 targetPos;
+    private Vector3 targetPos;
     public float speed = 2;
     public TextMeshProUGUI inventoryText;
+    public Animator animator;
+    public Rigidbody2D rb;
 
     void Start()
     {
+        targetPos = transform.position;
         int inventoryCount = PlayerPrefs.GetInt("count", 0);
         inventory = new Items[inventoryCount];
     }
@@ -49,21 +52,30 @@ public class PlayerScript : MonoBehaviour
     }
     void Update()
     {
-            Debug.Log(inventory.Length);
-            inventoryText.text = inventory.Length.ToString();
-
-
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(inventory.Length);
+        inventoryText.text = inventory.Length.ToString();
 
         if (Input.GetMouseButton(0))
         {
-            targetPos = new Vector2(mousePos.x, mousePos.y);
-
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = transform.position.z;
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+        Vector3 dir = targetPos - transform.position;
+        dir.Normalize();
 
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPos);
+        animator.SetFloat("Horizontal", dir.x);
+        animator.SetFloat("Vertical", dir.y);
+        animator.SetFloat("Magnitude", dir.magnitude);
+        if (dir.x == 0 && dir.y == 0)
+        {
+           animator.SetFloat("Magnitude", 0);
+        }
+
+        Debug.Log("X: " + dir.x);
+        Debug.Log("y: " + dir.y);
+        Debug.Log("magnitude: " + dir.magnitude);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
