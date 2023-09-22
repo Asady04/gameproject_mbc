19779ed9d42224b523b93a4b994973214919421b
Asady <fayyadh.asady04@gmail.com>
2023-09-22 22:08:42 +0700

    dialog with conversation

--- Assets/Programs/OpenDialog.cs
+++ Assets/Programs/OpenDialog.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class OpenDialog : MonoBehaviour
 {
@@ -10,13 +11,35 @@ public class OpenDialog : MonoBehaviour
     [SerializeField] GameObject barrier = null;
     [SerializeField] GameObject bar = null;
     [SerializeField] Button npc = null;
+    public TextMeshProUGUI conversation;
+    private int page;
+
+    public string npcName = "";
+
+    void Start()
+    {
+        npc.interactable = false;
+    }
     public void Open()
     {
+        page = 1;
         pause.SetActive(false);
         bar.SetActive(false);
         // string jenisBangun = gameObject.GetComponent<PlayerScript>().jenisBangun;
         dialog.SetActive(true);
         Time.timeScale = 0;
+        if (npcName == "elder")
+        {
+            Elder();
+        }
+        else if (npcName == "parent")
+        {
+            Parent();
+        }
+        else if (npcName == "merchant")
+        {
+            Merchant();
+        }
     }
 
     public void Close()
@@ -26,6 +49,71 @@ public class OpenDialog : MonoBehaviour
         dialog.SetActive(false);
         Time.timeScale = 1;
     }
+
+    void Elder()
+    {
+        if (page == 1)
+        {
+            conversation.text = "Laboris ex laborum ad commodo ullamco nulla proident consequat non adipisicing ea laboris. Nulla occaecat et incididunt incididunt enim. Sint ipsum occaecat pariatur reprehenderit velit velit. Excepteur nulla qui ipsum veniam occaecat aliquip aliquip. Enim do Lorem duis consectetur mollit aute.";
+        }
+        else if (page == 2)
+        {
+            conversation.text = "Do cupidatat dolore aliqua ipsum sunt fugiat dolore ut est. Excepteur ipsum tempor tempor dolore excepteur anim. Nulla non irure ut excepteur Lorem et pariatur. Ipsum veniam sunt voluptate magna enim Lorem cupidatat ipsum mollit cupidatat. Amet eu quis voluptate veniam excepteur ut veniam ea eiusmod veniam.";
+        }
+        else if (page == 3)
+        {
+            conversation.text = "Deserunt ipsum eiusmod consequat eu ut anim non duis quis sint qui amet dolore. In fugiat eiusmod excepteur anim dolor ullamco. Esse aliqua minim qui magna minim consectetur adipisicing anim nisi.";
+        }
+    }
+    void Parent()
+    {
+
+    }
+    void Merchant()
+    {
+
+    }
+
+    public void Next()
+    {
+        if (page < 3)
+        {
+            page++;
+        }
+        if (npcName == "elder")
+        {
+            Elder();
+        }
+        else if (npcName == "parent")
+        {
+            Parent();
+        }
+        else if (npcName == "merchant")
+        {
+            Merchant();
+        }
+
+    }
+
+    public void Back()
+    {
+        if (page > 1)
+        {
+            page--;
+        }
+        if (npcName == "elder")
+        {
+            Elder();
+        }
+        else if (npcName == "parent")
+        {
+            Parent();
+        }
+        else if (npcName == "merchant")
+        {
+            Merchant();
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Cek apakah yang masuk ke trigger adalah pemain.
@@ -34,7 +122,7 @@ public class OpenDialog : MonoBehaviour
             // pemainMendekati = true;
             npc.interactable = true;
             barrier.SetActive(true); // Munculkan tombol jika pemain mendekati objek.
-            
+
         }
     }
 
