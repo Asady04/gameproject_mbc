eb2946be62dd1e9de5aec17cba09a4c26473c8c5
xchweetz <nathanaeldwi2512@gmail.com>
2023-09-27 03:19:31 +0700

    animasi movement player 8 arah

--- Assets/Programs/PlayerMovement.cs
+++ Assets/Programs/PlayerMovement.cs
@@ -4,41 +4,20 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // Kecepatan pergerakan karakter
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rb2d;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        // Mengambil input dari pemain
-        float horizontalInput = 0f;
-        float verticalInput = 0f;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        // Mendeteksi input tombol "W", "A", "S", dan "D"
-        if (Input.GetKey("w"))
-        {
-            verticalInput = 1f;
-        }
-        if (Input.GetKey("a"))
-        {
-            horizontalInput = -1f;
-        }
-        if (Input.GetKey("s"))
-        {
-            verticalInput = -1f;
-        }
-        if (Input.GetKey("d"))
-        {
-            horizontalInput = 1f;
-        }
+        Vector2 movement = new Vector2(horizontalInput, verticalInput) * moveSpeed;
 
-        // Menghitung vektor pergerakan
-        Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized * moveSpeed;
-
-        // Menggerakkan karakter
-        rb.velocity = movement;
+        rb2d.velocity = movement;
     }
 }
